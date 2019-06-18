8a999e532ad51d9bd1abf01873534bed12f186ca
zhangjie <zhangjie@tcxa.com.cn>
2019-06-18 17:07:10 +0800

    change lib

--- vue.config.js
+++ vue.config.js
@@ -10,8 +10,9 @@ function resolve(dir) {
 
 module.exports = {
   publicPath: '/', // 部署应用时的根路径(默认'/'),也可用相对路径(存在使用限制)
-  outputDir: 'dist/static', // 运行时生成的生产环境构建文件的目录(默认''dist''，构建之前会被清除)
+  outputDir: 'dist', // 运行时生成的生产环境构建文件的目录(默认''dist''，构建之前会被清除)
   assetsDir: 'static', //放置生成的静态资源(s、css、img、fonts)的(相对于 outputDir 的)目录(默认'')
+  // indexPath: 'static/index.html',
   lintOnSave: process.env.NODE_ENV === 'development', // 是否在保存的时候检查
   productionSourceMap: false, // 生产环境是否生成 sourceMap 文件
   devServer: {
